057169adf450cc7dbed7a5d202fd382ff1c6cc0f
elikoga <elikoga@users.noreply.github.com>
2022-06-09 20:50:01 +0000

    deploy: a86a1e67fcac39e6aca52fb4296345eccfbf9f41

--- _app/chunks/en-8c0428fe.js
+++ _app/chunks/en-8c0428fe.js
@@ -1,7 +1,7 @@
-const t=`I'm currently a Student at the University of Paderborn studying Computer Science as a Bachelor.
+const t=`I'm currently a student at the University of Paderborn studying Computer Science as a Bachelor.
 
 I'm working at the [Flying Circus](https://flyingcircus.io/) doing mainly [Nix/NixOS](https://nixos.org/) and some [Python](https://www.python.org/) development right now.
 
-Outside of work I do Frontend Development with [Svelte](https://svelte.dev/) and backend development with [Rust](https://www.rust-lang.org/) or [Node.js](https://nodejs.org/).
+Outside of work I do frontend development with [Svelte](https://svelte.dev/) and backend development with [Rust](https://www.rust-lang.org/) or [Node.js](https://nodejs.org/).
 
 My R\xE9sum\xE9 is avaliable in [German](https://cv.eliko.ga/cv/de) and [English](https://cv.eliko.ga/cv/en).`,e="I build things, write code and solve problems.";var n={description:t,subtitle:e};export{n as default,t as description,e as subtitle};
