3160e43dfe724b2835645ce925313ef6bf2fb453
elikoga <elikoga@users.noreply.github.com>
2024-11-03 02:01:32 +0000

    deploy: 2109b96c23a9baa51694f9f8e4ec77d0789c05ad

---
+++ _app/chunks/en-03f3578b.js
@@ -0,0 +1,7 @@
+const t=`I'm currently a student at the University of Paderborn studying Computer Science as a Bachelor.
+
+I'm working at the [Flying Circus](https://flyingcircus.io/) doing mainly [Nix/NixOS](https://nixos.org/) and some [Python](https://www.python.org/) development right now.
+
+Right now I'm building [Thymis](https://thymis.io/), a device-management software for NixOS Devices.
+
+My R\xE9sum\xE9 is avaliable in [German](https://cv.eliko.ga/cv/de) and [English](https://cv.eliko.ga/cv/en).`,i="I build things, write code and solve problems.";var n={description:t,subtitle:i};export{n as default,t as description,i as subtitle};
