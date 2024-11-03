3e467574ff3aba53efdf0919a6fdd25a0357b527
elikoga <elikoga@users.noreply.github.com>
2024-11-03 02:02:30 +0000

    deploy: 200aed709fe32aa3b4fd5f48a1bf3c12475d28cd

---
+++ _app/chunks/de-c49f5af4.js
@@ -0,0 +1,10 @@
+const e=`Zurzeit studiere ich Informatik im Bachelor an der Universit\xE4t Paderborn.
+
+
+Ich arbeite aktuell beim [Flying Circus](https://flyingcircus.io/) und mache dort haupts\xE4chlich [Nix/NixOS](https://nixos.org/) und etwas [Python](https://www.python.org/) Entwicklung.
+
+
+Zurzeit baue ich an [Thymis](https://thymis.io/), einer Ger\xE4temanagement-Software f\xFCr NixOS Ger\xE4te.
+
+
+Mein Lebenslauf ist verf\xFCgbar in [Deutsch](https://cv.eliko.ga/cv/de) und [Englisch](https://cv.eliko.ga/cv/en).`,t="Ich baue Dinge, schreibe Code und l\xF6se Probleme.";var i={description:e,subtitle:t};export{i as default,e as description,t as subtitle};
